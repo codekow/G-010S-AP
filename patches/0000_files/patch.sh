7d4db15295d94410a6e6200c344426b9a1a69b23
Cory Latschkowski <koree@redhat.com>
2024-11-08 07:12:06 -0600

    update: file patches

--- patches/0000_files/patch.sh
+++ patches/0000_files/patch.sh
@@ -3,7 +3,17 @@ set -e
 
 ROOTFS=$1
 
+apply_patches(){
+  ls *.patch 2>/dev/null || return 0
+  for patch in *.patch
+  do
+    echo "Applying ${patch}..."
+    patch -p1 -d "$ROOTFS" < "${patch}"
+  done
+}
+
 delete_files(){
+  echo "Deleting unwanted files..."
   grep -v '^ *#' < delete.txt | while IFS= read -r file
   do
     [ -z "${file}" ] && continue
@@ -13,16 +23,20 @@ delete_files(){
 }
 
 sync_files(){
-  [ -d files ] || return 1
+  [ -d files ] || return 0
   echo "Syncing files..."
-  rsync -avH files/ "${ROOTFS}/"
+  rsync -avH --chown=root:root files/ "${ROOTFS}/"
 }
 
-patch_motd(){
-  [ -e "${ROOTFS}/motd" ] || return 1
-  echo "patched: $(date)" >> "${ROOTFS}/motd"
+patch_banner(){
+  [ -e "${ROOTFS}/etc/banner" ] || return 0
+  echo "Pathing /etc/banner..."
+  REV=$(git rev-parse --short HEAD) || REV=unknown
+  echo "patched: $(date)" >> "${ROOTFS}/etc/banner"
+  echo "commit: ${REV}" >> "${ROOTFS}/etc/banner"
 }
 
+apply_patches
 delete_files
 sync_files
-patch_motd
+patch_banner
