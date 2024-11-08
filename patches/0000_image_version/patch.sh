4acc0d19812176350fc5f32669765169e2b58049
Cory Latschkowski <koree@redhat.com>
2024-11-08 10:09:43 -0600

    update: patches

--- patches/0000_image_version/patch.sh
+++ patches/0000_image_version/patch.sh
@@ -6,9 +6,14 @@ ROOTFS=$1
 update_image_version(){
   if [ -z "${IMAGEVERSION}" ]; then
     echo "
+      ==========================================================
       IMAGEVERSION must be defined to set custom image version
-      example: 
+
+      Example: 
         export IMAGEVERSION='3FE45655BOCK99'
+          or
+        IMAGEVERSION='3FE45655BOCK99' patch.sh firmware/file.bin
+      ==========================================================
     "
     return 0
   fi
@@ -17,4 +22,21 @@ update_image_version(){
   sed -i.orig 's/IMAGEVERSION=.*/IMAGEVERSION='"${IMAGEVERSION}"'/g' "$ROOTFS/usr/etc/buildinfo"
 }
 
+create_image_override(){
+  [ ! -e "$ROOTFS/etc/init.d/update_image_version.sh" ] && return 0
+
+  cp update_image_version.sh "$ROOTFS/etc/init.d/update_image_version.sh"
+  echo "
+    ====================================================
+    Create /configs/image_version.lock after SFP boot
+      to force software versions
+      via file: /configs/image_version
+    
+    Example:
+      touch /configs/image_version.lock
+    ====================================================
+  "
+}
+
 update_image_version
+create_image_override
